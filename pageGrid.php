095709b19e8cb778afd9c5b8ad70e319385a1e17
Bricks System Development <brickswebcreative@gmail.com>
2024-08-08 07:23:53 +0800

    Deleting unneccesary files

--- pageGrid.php
+++
@@ -1,7 +0,0 @@
-<div class="page-grid-container">
-    <div class="nav-grid"></div>
-    <div class="filter-grid"></div>
-    <div class="product-content-grid"></div>
-    <div class="footer-grid"></div>
-</div>
-
